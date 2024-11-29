c71c6257846e01367930720f7933f31f0354bfb8
RAFF6629 <raff66craft@gmail.com>
2024-11-28 23:58:26 -0500

    Initial commit

---
+++ config.js
@@ -0,0 +1,45 @@
+global.owner = ['6283838956445']  
+global.mods = ['6283838956445'] 
+global.prems = ['6283838956445']
+global.nameowner = 'RAFF66'
+global.numberowner = '6283838956445' 
+global.mail = 'raff66info@gmail.com' 
+global.gc = 'https://chat.whatsapp.com/GNGKaHp3YhNKVbA7IjTzI7'
+global.instagram = 'https://instagram.com/RAFF66'
+global.wm = 'I M R A F F B O T'
+global.wait = '_*Tunggu sedang di proses...*_'
+global.eror = '_*Server Error*_'
+global.stiker_wait = '*⫹⫺ Stiker sedang dibuat...*'
+global.packname = 'Made With'
+global.author = 'RAFFBOT'
+global.autobio = false // Set true untuk mengaktifkan autobio
+global.maxwarn = '3' // Peringatan maksimum
+global.antiporn = true // Auto delete pesan porno (bot harus admin)
+
+//INI WAJIB DI ISI!//
+global.btc = 'hXVT6IoU' 
+//Daftar terlebih dahulu https://api.botcahx.eu.org
+
+//INI OPTIONAL BOLEH DI ISI BOLEH JUGA ENGGA//
+global.lann = 'UlDC56h9'
+//Daftar https://api.betabotz.eu.org 
+
+//jangan diganti!
+global.APIs = {   
+  btc: 'https://api.botcahx.eu.org'
+}
+
+//ini tidak di isi juga tidak apa-apa
+global.APIKeys = { 
+  'https://api.botcahx.eu.org': 'APIKEY' 
+}
+
+let fs = require('fs')
+let chalk = require('chalk')
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+  fs.unwatchFile(file)
+  console.log(chalk.redBright("Update 'config.js'"))
+  delete require.cache[file]
+  require(file)
+})
